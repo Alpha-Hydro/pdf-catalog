b1ad1ef661c5883459f81b136a63f96b7c059351
vladmeh <vladmeh@gmail.com>
2016-12-02 17:56:09 +0300

    create Bookmarks on field pages

--- module/Catalog/src/Catalog/Service/PdfService.php
+++ module/Catalog/src/Catalog/Service/PdfService.php
@@ -10,16 +10,22 @@
 namespace Catalog\Service;
 
 use TCPDF;
+use TCPDF_FONTS;
+use TCPDF_STATIC;
+use Zend\Debug\Debug;
 
 class PdfService extends TCPDF
 {
     protected $_widthWorkspacePage;
+
+    protected $_image_field_bookmark;
 
     public function __construct()
     {
         parent::__construct(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
         $this->setWidthWorkspacePage($this->getPageWidth()-(PDF_MARGIN_LEFT+PDF_MARGIN_RIGHT));
+        $this->setImageFieldBookmark('3.a4.small.png');
     }
 
     public function Header()
@@ -43,6 +49,8 @@ class PdfService extends TCPDF
 
         // paint a linear gradient
         $this->LinearGradient($this->lMargin, $this->y, $this->_widthWorkspacePage, 1, $blue, $white, $coords);
+
+        $this->fieldBookmarks();
     }
 
     public function Footer()
@@ -61,6 +69,210 @@ class PdfService extends TCPDF
     {
         parent::Output($name, $dest);
     }
+
+    public function addTOC($page='', $numbersfont='', $filler='.', $toc_name='TOC', $style='', $color=array(0,0,0)) {
+        $fontsize = $this->FontSizePt;
+        $fontfamily = $this->FontFamily;
+        $fontstyle = $this->FontStyle;
+        $w = $this->w - $this->lMargin - $this->rMargin;
+        $spacer = $this->GetStringWidth(chr(32)) * 4;
+        $lmargin = $this->lMargin;
+        $rmargin = $this->rMargin;
+        $x_start = $this->GetX();
+        $page_first = $this->page;
+        $current_page = $this->page;
+        $page_fill_start = false;
+        $page_fill_end = false;
+        $current_column = $this->current_column;
+        if (TCPDF_STATIC::empty_string($numbersfont)) {
+            $numbersfont = $this->default_monospaced_font;
+        }
+        if (TCPDF_STATIC::empty_string($filler)) {
+            $filler = ' ';
+        }
+        if (TCPDF_STATIC::empty_string($page)) {
+            $gap = ' ';
+        } else {
+            $gap = '';
+            if ($page < 1) {
+                $page = 1;
+            }
+        }
+        $this->SetFont($numbersfont, $fontstyle, $fontsize);
+        $numwidth = $this->GetStringWidth('00000');
+        $maxpage = 0; //used for pages on attached documents
+
+        foreach ($this->outlines as $key => $outline) {
+
+            //Debug::dump($current_column);
+            // check for extra pages (used for attachments)
+            if (($this->page > $page_first) AND ($outline['p'] >= $this->numpages)) {
+                $outline['p'] += ($this->page - $page_first);
+            }
+            if ($this->rtl) {
+                $aligntext = 'R';
+                $alignnum = 'L';
+            } else {
+                $aligntext = 'L';
+                $alignnum = 'R';
+            }
+            if ($outline['l'] == 0) {
+                $this->SetFont($fontfamily, $outline['s'].'B', $fontsize);
+            } else {
+                $this->SetFont($fontfamily, $outline['s'], $fontsize - $outline['l']);
+            }
+            $this->SetTextColorArray($outline['c']);
+            // check for page break
+            $this->checkPageBreak(2 * $this->getCellHeight($this->FontSize));
+            // set margins and X position
+            if (($this->page == $current_page) AND ($this->current_column == $current_column)) {
+                $this->lMargin = $lmargin;
+                $this->rMargin = $rmargin;
+            } else {
+                if ($this->current_column != $current_column) {
+                    if ($this->rtl) {
+                        $x_start = $this->w - $this->columns[$this->current_column]['x'];
+                    } else {
+                        $x_start = $this->columns[$this->current_column]['x'];
+                    }
+                }
+                $lmargin = $this->lMargin;
+                $rmargin = $this->rMargin;
+                $current_page = $this->page;
+                $current_column = $this->current_column;
+            }
+
+            $x_start = ($this->booklet)?$this->lMargin:$x_start;
+            $this->SetX($x_start);
+
+            $indent = ($spacer * $outline['l']);
+            if ($this->rtl) {
+                $this->x -= $indent;
+                $this->rMargin = $this->w - $this->x;
+            } else {
+                $this->x += $indent;
+                $this->lMargin = ($this->booklet)?$this->lMargin:$this->x;
+            }
+            $link = $this->AddLink();
+            $this->SetLink($link, $outline['y'], $outline['p']);
+            // write the text
+            if ($this->rtl) {
+                $txt = ' '.$outline['t'];
+            } else {
+                $txt = $outline['t'].' ';
+            }
+            $this->Write(0, $txt, $link, false, $aligntext, false, 0, false, false, 0, $numwidth, '');
+            if ($this->rtl) {
+                $tw = $this->x - $this->lMargin;
+            } else {
+                $tw = $this->w - $this->rMargin - $this->x;
+            }
+            $this->SetFont($numbersfont, $fontstyle, $fontsize);
+            if (TCPDF_STATIC::empty_string($page)) {
+                $pagenum = $outline['p'];
+            } else {
+                // placemark to be replaced with the correct number
+                $pagenum = '{#'.($outline['p']).'}';
+                if ($this->isUnicodeFont()) {
+                    $pagenum = '{'.$pagenum.'}';
+                }
+                $maxpage = max($maxpage, $outline['p']);
+            }
+            $fw = ($tw - $this->GetStringWidth($pagenum.$filler));
+            $wfiller = $this->GetStringWidth($filler);
+            if ($wfiller > 0) {
+                $numfills = floor($fw / $wfiller);
+            } else {
+                $numfills = 0;
+            }
+            if ($numfills > 0) {
+                $rowfill = str_repeat($filler, $numfills);
+            } else {
+                $rowfill = '';
+            }
+            if ($this->rtl) {
+                $pagenum = $pagenum.$gap.$rowfill;
+            } else {
+                $pagenum = $rowfill.$gap.$pagenum;
+            }
+
+            // write the number
+            $this->Cell($tw, 0, $pagenum, 0, 1, $alignnum, 0, $link, 0);
+        }
+        $page_last = $this->getPage();
+        $numpages = ($page_last - $page_first + 1);
+
+
+        if ($this->booklet) {
+            // check if a blank page is required before TOC
+            $page_fill_start = ((($page_first % 2) == 0) XOR (($page % 2) == 0));
+            $page_fill_end = (!((($numpages % 2) == 0) XOR ($page_fill_start)));
+            if ($page_fill_start) {
+                // add a page at the end (to be moved before TOC)
+                $this->addPage();
+                ++$page_last;
+                ++$numpages;
+            }
+            if ($page_fill_end) {
+                // add a page at the end
+                $this->addPage();
+                ++$page_last;
+                ++$numpages;
+            }
+
+        }
+        $maxpage = max($maxpage, $page_last);
+        if (!TCPDF_STATIC::empty_string($page)) {
+            for ($p = $page_first; $p <= $page_last; ++$p) {
+                // get page data
+                $temppage = $this->getPageBuffer($p);
+                for ($n = 1; $n <= $maxpage; ++$n) {
+                    // update page numbers
+                    $a = '{#'.$n.'}';
+                    // get page number aliases
+                    $pnalias = $this->getInternalPageNumberAliases($a);
+                    // calculate replacement number
+                    if (($n >= $page) AND ($n <= $this->numpages)) {
+                        $np = $n + $numpages;
+                    } else {
+                        $np = $n;
+                    }
+                    $na = TCPDF_STATIC::formatTOCPageNumber(($this->starting_page_number + $np - 1));
+                    $nu = TCPDF_FONTS::UTF8ToUTF16BE($na, false, $this->isunicode, $this->CurrentFont);
+                    // replace aliases with numbers
+                    foreach ($pnalias['u'] as $u) {
+                        $sfill = str_repeat($filler, max(0, (strlen($u) - strlen($nu.' '))));
+                        if ($this->rtl) {
+                            $nr = $nu.TCPDF_FONTS::UTF8ToUTF16BE(' '.$sfill, false, $this->isunicode, $this->CurrentFont);
+                        } else {
+                            $nr = TCPDF_FONTS::UTF8ToUTF16BE($sfill.' ', false, $this->isunicode, $this->CurrentFont).$nu;
+                        }
+                        $temppage = str_replace($u, $nr, $temppage);
+                    }
+                    foreach ($pnalias['a'] as $a) {
+                        $sfill = str_repeat($filler, max(0, (strlen($a) - strlen($na.' '))));
+                        if ($this->rtl) {
+                            $nr = $na.' '.$sfill;
+                        } else {
+                            $nr = $sfill.' '.$na;
+                        }
+                        $temppage = str_replace($a, $nr, $temppage);
+                    }
+                }
+                // save changes
+                $this->setPageBuffer($p, $temppage);
+            }
+            // move pages
+            $this->Bookmark($toc_name, 0, 0, $page_first, $style, $color);
+            if ($page_fill_start) {
+                $this->movePage($page_last, $page_first);
+            }
+            for ($i = 0; $i < $numpages; ++$i) {
+                $this->movePage($page_last, $page);
+            }
+        }
+    }
+
 
     public function defaultSettingsPage()
     {
@@ -110,33 +322,30 @@ class PdfService extends TCPDF
     public function tableOfContent($treeCategories)
     {
         //$this->AddPage();
+        $this->setImageFieldBookmark('3.active.a4.png');
         foreach ($treeCategories as $category1){
             $this->SetHeaderData('', 0, $category1['name'], '');
             $this->AddPage();
-            $this->Bookmark($category1['name'], 0 , 0, '', 'B', [237, 133, 31], $this->GetX());
+            $this->Bookmark($category1['name'], 0 , 0, '', 'B', [237, 133, 31]);
             //$this->Cell(0, 0, $category1['name'], 0, 1, 'L');
             if($category1['sub_categories']){
                 foreach ($category1['sub_categories'] as $category2){
                     //$this->AddPage();
-                    $this->Bookmark($category2['name'], 1 , 0, '', 'B', [0, 0, 0], $this->GetX());
+                    $this->Bookmark($category2['name'], 1 , 0, '', 'B', [0, 0, 0]);
                     //$this->Cell(0, 0, $category2['name'], 0, 1, 'L');
                     if($category2['sub_categories']){
                         foreach ($category2['sub_categories'] as $category3){
                             $this->AddPage();
-                            $this->Bookmark($category3['name'], 2 , 0, '', '', [0, 0, 0], $this->GetX());
+                            $this->Bookmark($category3['name'], 2 , 0, '', '', [0, 0, 0]);
                             $this->Cell(0, 0, $category3['name'], 0, 1, 'L');
                         }
                     }
                     else{
                         $this->AddPage();
                     }
-                    //$this->Bookmark($category2['name'], 1 , 0, '', 'B', [0, 0, 0]);
                 }
             }
-            //$this->Bookmark($category1['name'], 0 , 0, '', 'B', [237, 133, 31]);
         }
-
-        //$this->_setTableOfContent($treeCategories);
 
         $this->SetHeaderData('', 0, 'Содержание', '');
         $this->addTOCPage();
@@ -218,5 +427,42 @@ class PdfService extends TCPDF
         $this->SetX($this->x +3);
         $image_file = __DIR__ .'/../../../../../data/images/pdf/alfa-hydro.png';
         $this->Image($image_file, $this->x, $this->y -6, 50, '', 'PNG', '', 'M', true, 150, '', false, false, 0, false, false, false);
+    }
+
+    private function fieldBookmarks(){
+        $template_id = $this->startTemplate(60, 60, true);
+
+        $image_file = $this->getImageFieldBookmark();
+        if($this->getNumPages() % 2 === 0){
+            $this->Image($image_file, 0, 0, 15, 0, 'PNG', '', '', true, 150, '', false, false, 0, false, false, false);
+        }
+        else{
+            $this->StartTransform();
+            $this->MirrorH();
+            $this->Image($image_file, -15, 0, 15, 0, 'PNG', '', '', true, 150, '', false, false, 0, false, false, false);
+            $this->StopTransform();
+        }
+
+        $this->endTemplate();
+
+        $x = ($this->getNumPages() % 2 === 0)?$this->w - 15:0;
+
+        $this->printTemplate($template_id, $x, 20, 0, 0, '', '', false);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImageFieldBookmark()
+    {
+        return $this->_image_field_bookmark;
+    }
+
+    /**
+     * @param mixed $image_field_bookmark
+     */
+    public function setImageFieldBookmark($image_field_bookmark)
+    {
+        $this->_image_field_bookmark = __DIR__ .'/../../../../../data/images/pdf/'.$image_field_bookmark;
     }
 }
